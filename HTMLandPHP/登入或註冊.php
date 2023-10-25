b756979190cbd504974079a98f7b68db8bfe955f
avery30809 <avery2070527@gmail.com>
2023-10-26 01:41:27 +0800

    新增登入功能 並改變存檔位置

--- HTMLandPHP/登入或註冊.php
+++ HTMLandPHP/登入或註冊.php
@@ -1,6 +1,32 @@
+<?php
+    //登入後端
+    session_start();
+
+    if(isset($_POST['sign-in-submit'])){
+
+        $account = $_POST["account"];
+        $password = $_POST["password"];
+
+        require_once('database/connect.php');
+        $result = $conn->query("SELECT useraccount, pwd FROM userdata WHERE isAdmin = TRUE");
+        $admin = $result->fetch_assoc();
+
+        if($account == $admin["useraccount"] && $password == $admin["pwd"]) {
+            header("Location: AllClassRoom.html");
+            exit();
+        } 
+        $username = $conn->query("SELECT username FROM userdata WHERE useraccount = '$account' AND pwd = '$password'")->fetch_assoc();
+        if (!empty($username)) {
+            header("Location: AllClassRoom.html");
+            exit();
+        } else {
+            $_SESSION['Error']='Invalid account or password';
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,7 +39,6 @@
             /*讓內距不影響box長跟寬*/
             font-family: 'Montserrat', sans-serif;
         }
-
         /* 主頁面樣式 */
         body {
             /* background-color: #c9d6ff;
@@ -24,7 +49,6 @@
             flex-direction: column;
             height: 100vh;
         }
-
         /* 主容器樣式 */
         .container {
             background-color: #fff;
@@ -36,7 +60,6 @@
             max-width: 100%;
             min-height: 480px;
         }
-
         /* 簡單敘述的排版 */
         .container p {
             font-size: 14px;
@@ -44,13 +67,11 @@
             letter-spacing: 0.3px;
             margin: 20px 0;
         }
-
         .container a {
             color: #333;
             font-size: 13px;
             margin: 15px 0 10px;
         }
-
         /* 按鈕樣式 */
         .container button {
             /* background-color: #512da8;
@@ -76,7 +97,6 @@
             padding: 0 40px;
             height: 100%;
         }
-
         /* 輸入框樣式 */
         .container input {
             background-color: #eee;
@@ -88,7 +108,6 @@
             width: 100%;
             outline: none;
         }
-
         /* 表單容器樣式 */
         .form-container {
             position: absolute;
@@ -96,17 +115,14 @@
             height: 100%;
             transition: all 0.6s ease-in-out;
         }
-
         .sign-in {
             left: 0;
             width: 50%;
             z-index: 2;
         }
-
         .container.active .sign-in {
             transform: translateX(100%);
         }
-
         /* 註冊表單樣式 */
         .sign-up {
             left: 0;
@@ -114,15 +130,11 @@
             z-index: 1;
             /*分層，放在登入畫面後*/
         }
-
         /* 啟用註冊表單的樣式 */
         .container.active .sign-up {
             transform: translateX(100%);
-            opacity: 1;
             z-index: 5;
-            animation: move 0.6s;
         }
-
         /* 切換介面前容器樣式 */
         .toggle-container {
             position: absolute;
@@ -135,13 +147,11 @@
             border-radius: 150px 0 0 100px;
             z-index: 1000;
         }
-
         /* 啟用切換容器的樣式 */
         .container.active .toggle-container {
             transform: translateX(-100%);
             border-radius: 0 150px 100px 0;
         }
-
         /* 切換容器樣式 */
         .toggle {
             background-color: #5489d9;
@@ -155,13 +165,11 @@
             transform: translateX(0);
             transition: all 0.6s ease-in-out;
         }
-
         /* 啟用切換容器的樣式 */
         .container.active .toggle {
             transform: translateX(50%);
             /*左右對稱，各占50%*/
         }
-
         /* 切換面板樣式 */
         .toggle-panel {
             position: absolute;
@@ -177,23 +185,19 @@
             transform: translateX(0);
             transition: all 0.6s ease-in-out;
         }
-
         /* 往左側切換面板樣式 */
         .toggle-left {
             transform: translateX(-200%);
         }
-
         /* 啟用左側切換面板的樣式 */
         .container.active .toggle-left {
             transform: translateX(0);
         }
-
         /* 右側切換面板樣式 */
         .toggle-right {
             right: 0;
             transform: translateX(0);
         }
-
         /* 啟用右側切換面板的樣式 */
         .container.active .toggle-right {
             transform: translateX(200%);
@@ -206,7 +210,7 @@
         }
 
         .email-text {
-            width: 250px !important;
+            width: 146px !important;
         }
 
         .verification-button {
@@ -230,25 +234,14 @@
 <body>
     <div class="container" id="container">
 
-        <!-- 登入表單 -->
+        <!-- 註冊表單 -->
         <div class="form-container sign-up">
-            <form>
-                <h1>登入</h1>
-                <input type="email" placeholder="Email">
-                <input type="password" placeholder="密碼">
-                <a href="#">忘記密碼了嗎 ?</a>
-                <button>Sign In</button>
-            </form>
-        </div>
-
-        <!--註冊表單 -->
-        <div class="form-container sign-in">
-            <form>
+            <form method="POST">
                 <h1>註冊帳號</h1>
-                <br>
                 <input type="text" placeholder="姓名">
                 <div class="email">
-                    <input type="email" class="email-text" placeholder="Email">
+                    <input type="text" class="email-text" placeholder="學號">
+                    <span style="color: grey; position: relative; top: 27px;font-size: 50%;width: 100px">@mail.ntou.edu.tw</span>
                     <a href="AllClassRoom.html" target="_blank" class="verification-button">驗證</a>
                 </div>
                 <input type="text" placeholder="驗證碼">
@@ -259,44 +252,72 @@
             </form>
         </div>
 
+        <!-- 登入表單 -->
+        <div class="form-container sign-in">
+            <form method="POST">
+                <h1>登入</h1>
+                <?php
+                    if(isset($_SESSION["Error"])){
+                        echo '<h1 id="error-message" style="color: red; font-size: 90%">帳號或密碼錯誤</h1>';
+                        echo '<script>
+                            let element = document.getElementById("error-message");
+                            if(element) {
+                                element.style.display = "block";
+                                element.style.transition = "opacity 4s";
+                                element.style.opacity = 1;
+                                setTimeout(function() {
+                                    element.style.opacity = 0;
+                                }, 10);
+                                setTimeout(function() {
+                                    element.style.display = "none";
+                                }, 3000);
+                            }
+                        </script>';
+                        unset($_SESSION['Error']);
+                    }
+                ?>
+                <input type="text" name="account" placeholder="學號">
+                <input type="password" name="password" placeholder="密碼">
+                <a href="#">忘記密碼了嗎 ?</a>
+                <button type="submit" name="sign-in-submit">Sign In</button>
+            </form>
+        </div>
+
         <!-- 切換容器 -->
         <div class="toggle-container">
             <div class="toggle">
                 <!-- 切換容器中包含的兩個切換面板 -->
 
-                <!-- 左側切換面板 - 註冊 -->
+                <!-- 左側切換面板 - 登入 -->
                 <div class="toggle-panel toggle-left">
                     <h1>歡迎回來!</h1>
-                    <p>輸入你的個人信息已登入系統</p>
-                    <button class="hidden" id="register">註冊</button>
-                </div>
-
-                <!-- 右側切換面板 - 登入 -->
-                <div class="toggle-panel toggle-right">
-                    <h1>你好，使用者!</h1>
                     <p>填寫詳細資料已註冊帳號</p>
                     <button class="hidden" id="login">登入</button>
+                </div>
 
+                <!-- 右側切換面板 - 註冊 -->
+                <div class="toggle-panel toggle-right">
+                    <h1>你好，使用者!</h1>
+                    <p>輸入你的個人信息已登入系統</p>
+                    <button class="hidden" id="register">註冊</button>
                 </div>
             </div>
         </div>
     </div>
-
     <script>
         const container = document.getElementById('container');
         const registerBtn = document.getElementById('register');
 
         const loginBtn = document.getElementById('login');
 
-        loginBtn.addEventListener('click', () => {
+        registerBtn.addEventListener('click', () => {
             container.classList.add("active");
         });
 
 
-        registerBtn.addEventListener('click', () => {
+        loginBtn.addEventListener('click', () => {
             container.classList.remove("active");
         });
-
     </script>
 </body>
 
