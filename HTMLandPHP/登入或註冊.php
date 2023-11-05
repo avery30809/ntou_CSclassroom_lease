d908de19d5a3c549f813427be2b286a0ff9609b5
avery30809 <avery2070527@gmail.com>
2023-11-05 15:28:21 +0800

    新增傳送驗證碼

--- HTMLandPHP/登入或註冊.php
+++ HTMLandPHP/登入或註冊.php
@@ -23,6 +23,13 @@
             $_SESSION['Error']='Invalid account or password';
         }
     }
+    if (isset($_SESSION['verification_code']) && isset($_SESSION['verification_code_expiration'])) {
+        // 檢查是否過期
+        if (time() > $_SESSION['verification_code_expiration']) {
+            unset($_SESSION['verification_code']);
+            unset($_SESSION['verification_code_expiration']);
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -31,204 +38,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登入測試</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            /*讓內距不影響box長跟寬*/
-            font-family: 'Montserrat', sans-serif;
-        }
-        /* 主頁面樣式 */
-        body {
-            /* background-color: #c9d6ff;
-            background: linear-gradient(to right, #e2e2e2, #c9d6ff); 由左到右的漸變色 */
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            height: 100vh;
-        }
-        /* 主容器樣式 */
-        .container {
-            background-color: #fff;
-            border-radius: 30px;
-            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.35);
-            position: relative;
-            overflow: hidden;
-            width: 768px;
-            max-width: 100%;
-            min-height: 480px;
-        }
-        /* 簡單敘述的排版 */
-        .container p {
-            font-size: 14px;
-            line-height: 20px;
-            letter-spacing: 0.3px;
-            margin: 20px 0;
-        }
-        .container a {
-            color: #333;
-            font-size: 13px;
-            margin: 15px 0 10px;
-        }
-        /* 按鈕樣式 */
-        .container button {
-            /* background-color: #512da8;
-            color: #fff; */
-            font-size: 12px;
-            padding: 10px 45px;
-            border: 1px solid transparent;
-            border-radius: 8px;
-            font-weight: 600;
-            letter-spacing: 0.5px;
-            text-transform: uppercase;
-            margin-top: 10px;
-            cursor: pointer;
-        }
-
-        /* 表單樣式 */
-        .container form {
-            background-color: #fff;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            padding: 0 40px;
-            height: 100%;
-        }
-        /* 輸入框樣式 */
-        .container input {
-            background-color: #eee;
-            border: none;
-            margin: 8px 0;
-            padding: 10px 15px;
-            font-size: 13px;
-            border-radius: 8px;
-            width: 100%;
-            outline: none;
-        }
-        /* 表單容器樣式 */
-        .form-container {
-            position: absolute;
-            top: 0;
-            height: 100%;
-            transition: all 0.6s ease-in-out;
-        }
-        .sign-in {
-            left: 0;
-            width: 50%;
-            z-index: 2;
-        }
-        .container.active .sign-in {
-            transform: translateX(100%);
-        }
-        /* 註冊表單樣式 */
-        .sign-up {
-            left: 0;
-            width: 50%;
-            z-index: 1;
-            /*分層，放在登入畫面後*/
-        }
-        /* 啟用註冊表單的樣式 */
-        .container.active .sign-up {
-            transform: translateX(100%);
-            z-index: 5;
-        }
-        /* 切換介面前容器樣式 */
-        .toggle-container {
-            position: absolute;
-            top: 0;
-            left: 50%;
-            width: 50%;
-            height: 100%;
-            overflow: hidden;
-            transition: all 0.6s ease-in-out;
-            border-radius: 150px 0 0 100px;
-            z-index: 1000;
-        }
-        /* 啟用切換容器的樣式 */
-        .container.active .toggle-container {
-            transform: translateX(-100%);
-            border-radius: 0 150px 100px 0;
-        }
-        /* 切換容器樣式 */
-        .toggle {
-            background-color: #5489d9;
-            /* background: linear-gradient(to right, #5c6bc0, #512da8); */
-            height: 100%;
-            color: #fff;
-            position: relative;
-            left: -100%;
-            height: 100%;
-            width: 200%;
-            transform: translateX(0);
-            transition: all 0.6s ease-in-out;
-        }
-        /* 啟用切換容器的樣式 */
-        .container.active .toggle {
-            transform: translateX(50%);
-            /*左右對稱，各占50%*/
-        }
-        /* 切換面板樣式 */
-        .toggle-panel {
-            position: absolute;
-            width: 50%;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            padding: 0 30px;
-            text-align: center;
-            top: 0;
-            transform: translateX(0);
-            transition: all 0.6s ease-in-out;
-        }
-        /* 往左側切換面板樣式 */
-        .toggle-left {
-            transform: translateX(-200%);
-        }
-        /* 啟用左側切換面板的樣式 */
-        .container.active .toggle-left {
-            transform: translateX(0);
-        }
-        /* 右側切換面板樣式 */
-        .toggle-right {
-            right: 0;
-            transform: translateX(0);
-        }
-        /* 啟用右側切換面板的樣式 */
-        .container.active .toggle-right {
-            transform: translateX(200%);
-        }
-
-        .email {
-            display: flex;
-            flex-direction: row;
-            height: 50px;
-        }
-
-        .email-text {
-            width: 146px !important;
-        }
-
-        .verification-button {
-            width: 50px;
-            border-radius: 10px;
-            font-weight: 600;
-            color: white !important;
-            text-decoration: none;
-            font-size: 15px !important;
-            padding-top: 2px !important;
-            /* padding-bottom: 10px !important; */
-            padding-left: 11px;
-            padding-right: 1px;
-            margin-left: 8px !important;
-            background-color: rgba(220, 84, 84, 0.607);
-            color: white;
-        }
-    </style>
+    <link rel="stylesheet" href="CSS/登入或註冊.css">
 </head>
 
 <body>
@@ -240,9 +50,9 @@
                 <h1>註冊帳號</h1>
                 <input type="text" placeholder="姓名">
                 <div class="email">
-                    <input type="text" class="email-text" placeholder="學號">
+                    <input type="text" class="email-text" placeholder="學號" id="studentID">
                     <span style="color: grey; position: relative; top: 27px;font-size: 50%;width: 100px">@mail.ntou.edu.tw</span>
-                    <a href="AllClassRoom.html" target="_blank" class="verification-button">驗證</a>
+                    <button id="vertify" class="verification-button">驗證</button>
                 </div>
                 <input type="text" placeholder="驗證碼">
                 <input type="password" placeholder="密碼">
@@ -309,7 +119,7 @@
         const registerBtn = document.getElementById('register');
 
         const loginBtn = document.getElementById('login');
-
+        const vertifyBtn = document.getElementById('vertify');
         registerBtn.addEventListener('click', () => {
             container.classList.add("active");
         });
@@ -317,6 +127,21 @@
 
         loginBtn.addEventListener('click', () => {
             container.classList.remove("active");
+        });
+        vertifyBtn.addEventListener('click', () => {
+            event.preventDefault(); // 阻止表單預設的提交行為
+            const formData = new FormData();
+            const email = document.getElementById("studentID").value + "@mail.ntou.edu.tw";
+            formData.append("email", email);
+
+            fetch('sendemail/sender.php', {
+                method: 'POST',
+                body: formData
+            });
+            vertifyBtn.disabled = true;
+            setTimeout(()=>{
+                vertifyBtn.disabled = false; 
+            },5*60*1000);
         });
     </script>
 </body>
