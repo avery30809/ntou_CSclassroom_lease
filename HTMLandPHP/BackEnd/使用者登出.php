05db8f4dc4bd07e5c65ac5d0c78d92bae5e3f8dd
avery30809 <avery2070527@gmail.com>
2023-12-05 02:59:28 +0800

    統合到controller了

--- HTMLandPHP/BackEnd/使用者登出.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        //確保已經登入
-        if(isset($_SESSION["userID"])) {
-            unset($_SESSION["userID"]);
-        }
-    }
-?>
--- HTMLandPHP/BackEnd/獲取教室名稱.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if($_SERVER['REQUEST_METHOD'] == "POST") {
-        require_once("database/connect.php");
-        $result = $conn->query("SELECT RoomName FROM classrooms");
-        $classrooms = $result->fetch_all();
-        echo json_encode($classrooms);
-    }
-?>
