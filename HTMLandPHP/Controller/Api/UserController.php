7ff1db8a4a2c98677cde54f4d198622495628a02
avery30809 <avery2070527@gmail.com>
2023-12-06 01:54:52 +0800

    刪除不必要的判斷式

--- HTMLandPHP/Controller/Api/UserController.php
+++ HTMLandPHP/Controller/Api/UserController.php
@@ -55,16 +55,14 @@ class UserController extends BaseController
     }
     //搜尋日期
     private function handleSearchDate() {
-        if($_SERVER['REQUEST_METHOD'] == "POST") {
-            if(isset($_POST["date"])) {
-                $_SESSION["date"] = $_POST["date"];
-            }
-            else if(isset($_SESSION["date"])){
-                $obj = [
-                    "date" => $_SESSION["date"]
-                ];
-                $this->sendOutput(json_encode($obj));
-            }
+        if(isset($_POST["date"])) {
+            $_SESSION["date"] = $_POST["date"];
+        }
+        else if(isset($_SESSION["date"])){
+            $obj = [
+                "date" => $_SESSION["date"]
+            ];
+            $this->sendOutput(json_encode($obj));
         }
     }
     //登入
