ac4944fba6021177bdb7006eed4e132bf4d9ee57
avery30809 <avery2070527@gmail.com>
2023-12-11 03:20:27 +0800

    新增動態更新借用情況的bar 以及 新增可以觀看該教室本周的借用情況

--- HTMLandPHP/Assets/JS/index.js
+++ HTMLandPHP/Assets/JS/index.js
@@ -74,7 +74,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
         sBtn_text = optionMenu.querySelector(".sBtn-text"),
         queryBlank = document.querySelector(".blank"),
         queryClassRoom = document.querySelector(".inputClassRoomName"),
-        queryTimeInterval = document.querySelector(".inputTimeInterval");
+        queryTimeInterval = document.querySelector(".inputTimeInterval"),
+        popup = document.getElementById("popup"),
+        dropdownStart = document.querySelector(".dropdownStart"),
+        dropdownEnd = document.querySelector(".dropdownEnd");
 
     //以後會被資料庫所取代
     let allClassroomName = [];
@@ -123,23 +126,54 @@ document.addEventListener("DOMContentLoaded", ()=>{
         activities[room].forEach((activity)=>{
             if(activity == "") count++;
         });
-        return `${(100-(count*100/startTime.length)).toFixed(2)}%`;
+        return (100-(count*100/startTime.length)).toFixed(2);
     }
 
     //查詢結果格式
     function showResult(room) {
-        return `<span onclick="showPopupClassroom(event)" class="classroomName">${room}</span><span class="gap">${room.slice(0, 3) == "INS" ? "電資大樓" : "電綜大樓"}</span><div class="totalBar"><div class="nowState-bar">${roomCondition(room)}</div></div><div class="wrap"></div>`
+        return `<span><span class="classroomName">${room}</span></span><span>${room.slice(0, 3) == "INS" ? "電資大樓" : "電綜大樓"}</span><span class="totalBar"><div class="nowState-bar">${roomCondition(room)}%</div></span><span class="borrowSpace"><button class="borrow">借用</button></span><div class="wrap"></div>`;
+    }
+    function getColorByPercentage(percentage) {
+        percentage = parseFloat(percentage);
+        let saturation = 70; // 控制飽和度
+        let lightness = 50; // 控制亮度
+        
+        let hue;
+        if (percentage <= 50) {
+            hue = Math.round(120 - (percentage / 50) * 60); // 綠到黃
+        } else {
+            hue = Math.round(120 - percentage * 6 / 5); // 黃到紅
+        }
+        // hue 120-綠 60-黃 0-紅
+        return 'hsl(' + hue + ',' + saturation + '%,' + lightness + '%)';
+    }
+    //加入點擊借用的監聽器
+    function addBorrowListener() {
+        let elements = document.getElementsByClassName("borrow");
+        for(let i=0; i<elements.length; i++) {
+            elements[i].addEventListener("click", showPopupClassroom, false);
+        }
+    }
+    //加入點擊教室名稱的監聽器
+    function addNameListenter() {
+        let elements = document.getElementsByClassName("classroomName");
+        for(let i=0; i<elements.length; i++) {
+            elements[i].addEventListener("click", showPopupCourse, false);
+        }
     }
 
     //列出所有教室狀態
     function showDefault() {
         let content = "";
-        content += '<span style="width: 70px;">教室名稱</span><span class="gap">教室位置</span><span>借用情況</span><div class="wrap"></div>';
+        content += '<span>教室名稱</span><span>教室位置</span><span>借用情況</span><div class="wrap"></div>';
 
         allClassroomName.forEach((classroom) => {
             content += showResult(classroom);
         });
         document.getElementById("showClassroom").innerHTML = content;
+        setBarColor();
+        addBorrowListener();
+        addNameListenter();
     }
 
     //下拉式選單
@@ -164,32 +198,30 @@ document.addEventListener("DOMContentLoaded", ()=>{
             })
         })
     })
-    document.getElementById("time1").addEventListener("click", () => {
+    document.getElementById("time1").addEventListener("click", (event) => {
         event.stopPropagation();
-        let temp = document.querySelector(".dropdownStart");
-        temp.style.display = temp.style.display === "block" ? "none" : "block";
-        document.querySelector(".dropdownEnd").style.display = "none";
+        dropdownStart.classList.toggle("active");
+        dropdownEnd.classList.remove("active");
         optionMenu.classList.remove("active");
+        popup.classList.remove("open-popup");
     })
-    document.getElementById("time2").addEventListener("click", () => {
+    document.getElementById("time2").addEventListener("click", (event) => {
         event.stopPropagation();
-        let temp = document.querySelector(".dropdownEnd");
-        temp.style.display = temp.style.display === "block" ? "none" : "block";
-        document.querySelector(".dropdownStart").style.display = "none";
+        dropdownEnd.classList.toggle("active");
+        dropdownStart.classList.remove("active");
         optionMenu.classList.remove("active");
+        popup.classList.remove("open-popup");
     })
     //監聽所有點擊事件 關閉彈跳視窗與下拉式選單
     document.addEventListener("click", (event) => {
-        let a = document.querySelector(".dropdownStart"),
-            b = document.querySelector(".dropdownEnd");
-        a.style.display = "none";
-        b.style.display = "none";
+        dropdownStart.classList.remove("active");
+        dropdownEnd.classList.remove("active");
         optionMenu.classList.remove("active");
         popup.classList.remove("open-popup");
     })
 
     //顯示以教室名稱或特定時段搜尋的結果
-    form.addEventListener("submit", () => {
+    form.addEventListener("submit", (event) => {
         event.preventDefault();
         let queryStartTime = document.getElementById("time1").value,
             queryEndTime = document.getElementById("time2").value;
@@ -201,15 +233,18 @@ document.addEventListener("DOMContentLoaded", ()=>{
             for (let i = 0; i < allClassroomName.length; i++) {
                 if (allClassroomName[i].startsWith(queryClassRoom.value)) properClassroomName.push(allClassroomName[i]);
             }
-            let content = '<span style="width: 70px;">教室名稱</span><span class="gap">教室位置</span><span>借用情況</span><div class="wrap"></div>';
+            let content = '<span>教室名稱</span><span>教室位置</span><span>借用情況</span><div class="wrap"></div>';
             properClassroomName.forEach((roomName)=>{
                 content += showResult(roomName);
             },false);
             queryClassRoom.value = "";
             document.getElementById("showClassroom").innerHTML = content;
+            setBarColor();
+            addBorrowListener();
+            addNameListenter();
         }
         else if (queryStartTime != "" && queryEndTime != "") {
-            let content = '<span style="width: 70px;">教室名稱</span><span class="gap">教室位置</span><span>借用情況</span><div class="wrap"></div>';
+            let content = '<span>教室名稱</span><span>教室位置</span><span>借用情況</span><div class="wrap"></div>';
             const properClassroomName = new Set();
             for(let i=startTime.findIndex((element)=>element == queryStartTime); i<=endTime.findIndex((element)=>element == queryEndTime); i++) {
                 allClassroomName.forEach((room)=>{
@@ -222,12 +257,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     content += showResult(roomName);
             },false);
             document.getElementById("showClassroom").innerHTML = content;
+            setBarColor();
+            addBorrowListener();
+            addNameListenter();
         }
     })
 
-    selectBtn.addEventListener("click", () => {
+    function setBarColor() {
+        let barElements = document.querySelectorAll(".nowState-bar");
+        barElements.forEach((element)=>{
+            let percentage = element.innerHTML;
+            element.style.background = getColorByPercentage(percentage);
+            element.style.width = `${percentage}`;
+        })
+    }
+
+    selectBtn.addEventListener("click", (event) => {
         event.stopPropagation();
         optionMenu.classList.toggle("active");
+        dropdownStart.classList.remove("active");
+        dropdownEnd.classList.remove("active");
+        popup.classList.remove("open-popup");
     });
 
     options.forEach(option => {
@@ -257,21 +307,108 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 document.querySelector(".queryButton").setAttribute("disabled", true);
                 showDefault();
             }
-
-            optionMenu.classList.remove("active");
         })
     })
 
     //彈跳視窗
-    let popup = document.getElementById("popup");
     popup.addEventListener("click", (event)=>{event.stopPropagation();},false);
 
     // 點擊教室開啟彈跳視窗的函數
     function showPopupClassroom(event) {
         event.stopPropagation();
         popup.classList.add("open-popup");
-        let roomName = event.target.innerHTML;
+        optionMenu.classList.remove("active");
+        dropdownStart.classList.remove("active");
+        dropdownEnd.classList.remove("active");
+        popup.style.width = "600px";
+        let roomName = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.getElementsByClassName("classroomName")[0].innerHTML;
         document.getElementById("popupName").innerHTML = roomName;
+        document.querySelector(".content").innerHTML = `
+        <span>起始時間</span><span>結束時間</span><span>借用狀態</span>
+        <div class="wrap"></div>
+        <span>8 a.m.</span><span>9 a.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>9 a.m.</span><span>10 a.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>10 a.m.</span><span>11 a.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>11 a.m.</span><span>12 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>12 p.m.</span><span>13 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>13 p.m.</span><span>14 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>14 p.m.</span><span>15 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>15 p.m.</span><span>16 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>16 p.m.</span><span>17 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>17 p.m.</span><span>18 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>18 p.m.</span><span>19 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>
+        <span>19 p.m.</span><span>20 p.m.</span>
+        <span><span
+            class="classroomState"></span></span>
+        <div class="wrap"></div>`;
+        let states = document.getElementsByClassName("classroomState");
+        for(let i=0; i<states.length; i++) {
+            if(activities[roomName][i] == "") {
+                states[i].innerHTML = "未借用";
+                states[i].classList.remove("borrowed");
+                states[i].addEventListener("click", applicationForm, false)
+            }
+            else {
+                states[i].innerHTML = activities[roomName][i];
+                states[i].classList.add("borrowed");
+                states[i].removeEventListener("click", applicationForm, false);
+            }
+        }
+    }
+
+    function showPopupCourse(event) {
+        event.stopPropagation();
+        popup.classList.add("open-popup");
+        optionMenu.classList.remove("active");
+        dropdownStart.classList.remove("active");
+        dropdownEnd.classList.remove("active");
+        popup.style.width = "1000px";
+        let roomName = event.target.innerHTML;
+        document.getElementById("popupName").innerHTML = roomName + "課表";
+        let myFrame = document.createElement("iframe");
+        myFrame.src = "Course.html";
+        myFrame.style.width = '100%';
+        myFrame.style.height = '600px';
+        let content = document.querySelector(".content");
+        content.innerHTML = "";
+        content.appendChild(myFrame);
+    }
+
+    function applicationForm() {
+        window.location.href = "Application form.html";
     }
 
     function closePopupClassroom() {
